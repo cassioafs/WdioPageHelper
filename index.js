e830bc07d98c5a384f520553427f543b8d943652
Cássio Alves <cassiocafs@gmail.com>
2018-03-21 14:27:14 -0300

    eslint

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 const TIME_OUT_DEFAULT = 5000;
 
 class Page {
-   
-     clickOn( element, timeout=TIME_OUT_DEFAULT ){
-       browser.waitForVisible( element,timeout );
-       browser.click( element );
-     }
-   
-     sendValue( element, value, timeout=TIME_OUT_DEFAULT ){
-       browser.waitForVisible( element, timeout );
-       browser.setValue( element, value );
-     }
-   
-     getValue(element, timeout = TIME_OUT_DEFAULT) {
-       browser.waitForVisible(element, timeout);
-       browser.getValue(element)
-     }
-   
-     isElementVisible( element, reverse = false, timeout=TIME_OUT_DEFAULT ){
-       browser.waitForVisible( element,timeout, reverse );
-       return browser.isVisible( element );
-    }
+  clickOn(element, timeout = TIME_OUT_DEFAULT) {
+    browser.waitForVisible(element, timeout);
+    browser.click(element);
+  }
+
+  sendValue(element, value, timeout = TIME_OUT_DEFAULT) {
+    browser.waitForVisible(element, timeout);
+    browser.setValue(element, value);
+  }
+
+  getValue(element, timeout = TIME_OUT_DEFAULT) {
+    browser.waitForVisible(element, timeout);
+    browser.getValue(element);
+  }
+
+  isElementVisible(element, reverse = false, timeout = TIME_OUT_DEFAULT) {
+    browser.waitForVisible(element, timeout, reverse);
+    return browser.isVisible(element);
+  }
 }
 module.exports = Page;
